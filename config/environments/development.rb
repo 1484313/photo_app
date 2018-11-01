9ea84ee6b33457eaf991238f68526e795361f7ba
Kamil <1484313@gmail.com>
2018-11-01 19:25:09 +0100

    fixed mailer & payment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,10 +8,10 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'http://0.0.0.0:3000' }
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
